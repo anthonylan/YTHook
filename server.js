82aca9ccb4852f1c771ca2620b9cf43109e23991
anthonylan <anthonylannn@gmail.com>
2019-12-21 15:30:02 +0100

    g

--- server.js
+++ server.js
@@ -51,8 +51,6 @@ router.get('/hook.js', (req, res) => {
 })
 
 //==================//
-const port = 4040;
-
 app.listen(process.env.PORT || 3000, function(){
   console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
